d1f049e79fd0b57eb354f74dc84aa6f229706977
nuptivites <nuptivites@gmail.com>
2020-10-14 07:32:23 -0400

    Improved the contact us page to include a picture and some styling to look more professional. Added contact_us.js to program the phone number properly. Added all auth

--- innocelf/innocelf/settings.py
+++ innocelf/innocelf/settings.py
@@ -38,14 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # Own apps
     'innoservices',
 
     # All auth apps
     'crispy_forms',
-    # 'allauth',
-    # 'allauth.account'
+    'allauth',
+    'allauth.account'
 ]
 
 MIDDLEWARE = [
@@ -78,6 +79,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'innocelf.wsgi.application'
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- innocelf/innoservices/forms.py
+++ innocelf/innoservices/forms.py
@@ -17,33 +17,34 @@ class ContactUsForm(forms.ModelForm):
 
         widgets = {
             'first_name': forms.TextInput(attrs={
-                'class': 'form-control lato-regular',
+                'class': 'form-control lato-regular bg-transparent text-white',
                 'id': 'contact_us_first_name',
                 'placeholder': 'First Name'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': 'form-control lato-regular',
+                'class': 'form-control lato-regular bg-transparent text-white',
                 'id': 'contact_us_last_name',
                 'placeholder': 'Last Name'
             }),
             'email': forms.EmailInput(attrs={
-                'class': 'form-control lato-regular',
+                'class': 'form-control lato-regular bg-transparent text-white',
                 'id': 'contact_us_email',
                 'placeholder': 'your_email@domain.com'
             }),
             'phone': forms.TextInput(attrs={
-                'class': 'form-control lato-regular',
+                'class': 'form-control lato-regular bg-transparent text-white',
                 'id': 'contact_us_phone',
                 'placeholder': 'XXX-XXX-XXXX',
                 'pattern': '[0-9]{3}-[0-9]{3}-[0-9]{4}'
             }),
             'inquiry_reason': forms.Select(attrs={
-                'class': 'custom-select custom-select-md lato-regular',
+                'class': 'custom-select custom-select-md lato-regular bg-transparent text-white',
                 'id': 'contact_us_reason'
             }),
             'explanation': forms.Textarea(attrs={
-                'class': 'form-control lato-regular',
+                'class': 'form-control lato-regular bg-transparent text-white',
                 'id': 'contact_explanation',
+                'rows': '3',
                 'placeholder': 'Please provide a brief summary of your inquiry. Please DO NOT provide confidential information.',
             }),
         }
