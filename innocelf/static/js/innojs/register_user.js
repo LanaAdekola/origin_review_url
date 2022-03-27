c9c1e334830ff8709ac814b3d05ceb76c688b384
nuptivites <nuptivites@gmail.com>
2022-03-27 15:26:42 -0400

    Deleted a bunch of static files from master

--- innocelf/static/js/innojs/register_user.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { addDashestoPhoneNumber } from './_global_functions.js';
-
-let phoneNumberTextInputId = 'register_user_phone_number';
-addDashestoPhoneNumber(phoneNumberTextInputId);
